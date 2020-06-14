8c9c1cc154e1f159b90335aee52b97ef46386d4e
changharry <harrychangzh@gmail.com>
2020-06-14 03:12:57 -0700

    Container With Most Water

---
+++ src/test/easy/ContainerWithMostWaterTest.java
@@ -0,0 +1,21 @@
+package test.easy;
+
+import easy.ContainerWithMostWater;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerWithMostWaterTest {
+
+    private ContainerWithMostWater obj;
+    @BeforeEach
+    void setUp() {
+        obj = new ContainerWithMostWater();
+    }
+
+    @Test
+    void maxArea() {
+        assertEquals(obj.maxArea(new int[]{1,8,6,2,5,4,8,3,7}), 49);
+    }
+}
