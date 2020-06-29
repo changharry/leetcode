24074aa118a593790f6b1d5b74db63a1431acddf
changharry <harrychangzh@gmail.com>
2020-06-29 13:09:42 -0700

    RemoveElement

---
+++ src/easy/RemoveElement.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        return n;
+    }
+}
---
+++ src/test/easy/RemoveElementTest.java
@@ -0,0 +1,23 @@
+package test.easy;
+
+import easy.RemoveElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveElementTest {
+
+    private RemoveElement obj;
+
+    @BeforeEach
+    void setUp() {
+        obj =  new RemoveElement();
+    }
+
+    @Test
+    void removeElement() {
+        assertEquals(obj.removeElement(new int[]{3,2,2,3}, 3), 2);
+        assertEquals(obj.removeElement(new int[]{0,1,2,2,3,0,4,2}, 2), 5);
+    }
+}
