d55deec44fabd69d71210a37ce5b8debf9fb86b1
changharry <harrychangzh@gmail.com>
2020-07-16 10:52:25 -0700

    IntegerToRomanTest add tests

--- src/test/medium/IntegerToRomanTest.java
+++ src/test/medium/IntegerToRomanTest.java
@@ -16,8 +16,10 @@ class IntegerToRomanTest {
 
     @Test
     void intToRoman() {
+        assertEquals(obj.intToRoman(1), "I");
         assertEquals(obj.intToRoman(3), "III");
         assertEquals(obj.intToRoman(4), "IV");
+        assertEquals(obj.intToRoman(6), "VI");
         assertEquals(obj.intToRoman(9), "IX");
         assertEquals(obj.intToRoman(58), "LVIII");
         assertEquals(obj.intToRoman(1994), "MCMXCIV");
