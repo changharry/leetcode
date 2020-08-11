12c96ee30e4d67ee881e0645e92f971c6049c073
changharry <harrychangzh@gmail.com>
2020-08-11 00:27:21 -0700

    MaximumSubarray add test

--- src/test/easy/MaximumSubarrayTest.java
+++ src/test/easy/MaximumSubarrayTest.java
@@ -4,6 +4,9 @@ import easy.MaximumSubarray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaximumSubarrayTest {
@@ -18,6 +21,7 @@ class MaximumSubarrayTest {
     @Test
     void maxSubArray() {
         assertEquals(obj.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}), 6);
+        assertEquals(obj.maxSubArray(new int[] {0}), 0);
         assertEquals(obj.maxSubArray(new int[]{1}), 1);
         assertEquals(obj.maxSubArray(new int[]{-2, 1}), 1);
     }
